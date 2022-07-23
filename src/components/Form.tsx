3c214adee6fbcc6cb90e93daafb1d1c4d22c6c5b
jheave <jimenezjheave123@gmail.com>
2022-07-24 02:09:54 +0800

    redesign form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,23 +1,62 @@
 import {
+    Box,
     Button,
+    Flex,
     FormControl,
+    FormLabel,
+    Heading,
     Input,
-    Stack
-} from '@chakra-ui/react'
+    Link,
+    Stack,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react';
 
-const Form= () => {
+export default function Form() {
     return (
-        <form>
-            <Stack spacing={4}>
-                <FormControl isRequired>
-                    <Input placeholder={"First name"} />
-                    <Input placeholder={"Last name"} />
-                    <Input placeholder={"Course"} />
-                    <Button type={"submit"}>Submit</Button>
-                </FormControl>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+                <Stack align={'center'}>
+                    <Heading fontSize={'2xl'}>Create Your Decentralized Certification</Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        to enjoy all of our cool <Link color={'blue.400'}>features</Link> ✌️
+                    </Text>
+                </Stack>
+                <Box
+                    rounded={'lg'}
+                    bg={useColorModeValue('white', 'gray.700')}
+                    boxShadow={'lg'}
+                    p={8}>
+                    <Stack spacing={4}>
+                        <FormControl id="first-name">
+                            <FormLabel>First Name</FormLabel>
+                            <Input type="tex" />
+                        </FormControl>
+                        <FormControl id="last-name">
+                            <FormLabel>Last Name</FormLabel>
+                            <Input type="text" />
+                        </FormControl>
+                        <FormControl id="course">
+                            <FormLabel>Course</FormLabel>
+                            <Input type="text" />
+                        </FormControl>
+                        <Stack spacing={10}>
+                            <Button
+                                bg={'blue.400'}
+                                color={'white'}
+                                _hover={{
+                                    bg: 'blue.500',
+                                }}>
+                               Generate Certificate
+                            </Button>
+                        </Stack>
+                    </Stack>
+                </Box>
             </Stack>
-        </form>
-    )
+        </Flex>
+    );
 }
-
-export default Form
