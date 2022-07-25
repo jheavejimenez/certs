054e8618362f1a980b2d36d60f1e16364cbc528c
jheave <jimenezjheave123@gmail.com>
2022-07-25 15:23:25 +0800

    fix design

--- src/Pages/Approver.tsx
+++ src/Pages/Approver.tsx
@@ -7,48 +7,54 @@ import {
     Tbody,
     Td,
     Th,
-    Tfoot,
+    Tfoot, Flex,
+    useColorModeValue,
 } from "@chakra-ui/react";
 
 
 export default function Approver() {
     return (
-
-        <TableContainer>
-            <Table variant='striped' colorScheme='teal'>
-                <TableCaption>Imperial to metric conversion factors</TableCaption>
-                <Thead>
-                    <Tr>
-                        <Th>To convert</Th>
-                        <Th>into</Th>
-                        <Th isNumeric>multiply by</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                    <Tr>
-                        <Td>inches</Td>
-                        <Td>millimetres (mm)</Td>
-                        <Td isNumeric>25.4</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>feet</Td>
-                        <Td>centimetres (cm)</Td>
-                        <Td isNumeric>30.48</Td>
-                    </Tr>
-                    <Tr>
-                        <Td>yards</Td>
-                        <Td>metres (m)</Td>
-                        <Td isNumeric>0.91444</Td>
-                    </Tr>
-                </Tbody>
-                <Tfoot>
-                    <Tr>
-                        <Th>To convert</Th>
-                        <Th>into</Th>
-                        <Th isNumeric>multiply by</Th>
-                    </Tr>
-                </Tfoot>
-            </Table>
-        </TableContainer>
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50', 'gray.800')}>
+            <TableContainer>
+                <Table variant='striped' colorScheme='blue'>
+                    <TableCaption>Imperial to metric conversion factors</TableCaption>
+                    <Thead>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                        <Tr>
+                            <Td>inches</Td>
+                            <Td>millimetres (mm)</Td>
+                            <Td isNumeric>25.4</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>feet</Td>
+                            <Td>centimetres (cm)</Td>
+                            <Td isNumeric>30.48</Td>
+                        </Tr>
+                        <Tr>
+                            <Td>yards</Td>
+                            <Td>metres (m)</Td>
+                            <Td isNumeric>0.91444</Td>
+                        </Tr>
+                    </Tbody>
+                    <Tfoot>
+                        <Tr>
+                            <Th>To convert</Th>
+                            <Th>into</Th>
+                            <Th isNumeric>multiply by</Th>
+                        </Tr>
+                    </Tfoot>
+                </Table>
+            </TableContainer>
+        </Flex>
     )
 }
