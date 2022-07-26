ee742dd09c662c5185ebb50f7e098a16b608cbd6
jheave <jimenezjheave123@gmail.com>
2022-07-27 00:38:59 +0800

    optimize import

--- src/utils/apiConfigs.ts
+++ src/utils/apiConfigs.ts
@@ -1,5 +1,5 @@
 import axios, {AxiosInstance} from 'axios';
-const issuerBaseUrl: string = `https://affinity-issuer.prod.affinity-project.org/api/v1/vc/build-unsigned`
+export const issuerBaseUrl: string = `https://affinity-issuer.prod.affinity-project.org/api/v1/vc/build-unsigned`
 const accessApiKey: string = `${process.env.API_KEY_HASH}`
 
 export const issuerApi: AxiosInstance = axios.create({
