48e64be44bc3efc3be67d988deb67a8c3826bac8
jheave <jimenezjheave123@gmail.com>
2022-07-26 15:29:01 +0800

    add api config

--- src/utils/apiConfigs.ts
+++ src/utils/apiConfigs.ts
@@ -1,11 +1,11 @@
 import axios, {AxiosInstance} from 'axios';
 const issuerBaseUrl: string = `https://affinity-issuer.prod.affinity-project.org/api/v1/vc/build-unsigned`
-
+const accessApiKey: string = `${process.env.API_KEY_HASH}`
 
 export const issuerApi: AxiosInstance = axios.create({
     baseURL: issuerBaseUrl,
     headers: {
-        'Api-Key': "",
+        'Api-Key': accessApiKey,
         'Content-Type': 'application/json',
     },
 });
