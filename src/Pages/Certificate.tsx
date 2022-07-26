71a9152df49e47df3982b9eec7744a39e2afb205
jheave <jimenezjheave123@gmail.com>
2022-07-26 17:41:10 +0800

    fix bug in condition checking

--- src/Pages/Certificate.tsx
+++ src/Pages/Certificate.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
 export const Certificate: React.FC<{  }> = props => {
-    const isApproved = localStorage.getItem("isApproved");
+   const isApproved = JSON.parse(localStorage.getItem("cert") || "{}").isApproved;
     return (
         <div>
-            {isApproved === "true" ? <div>Certificate</div> : <div>Please wait for approval</div>}
+            {isApproved === true ? <div>Certificate</div> : <div>Please wait for approval</div>}
         </div>
     );
 };
