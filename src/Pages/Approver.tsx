1faee4166da8baaf87b281767ed25ff1f0eec8b8
jheave <jimenezjheave123@gmail.com>
2022-08-03 00:10:13 +0800

    add todo for tomorrow

--- src/Pages/Approver.tsx
+++ src/Pages/Approver.tsx
@@ -7,34 +7,39 @@ export default function Approver() {
     const certs = [JSON.parse(localStorage.getItem("cert") || "{}")];
     const filteredCerts = certs.filter(cert => !cert.isApproved);
 
+    console.log(process.env.REACT_APP_DID);
+
     const handleApprove = async (cert: any) => {
-        // const body = {
-        //     "jsonLdContextUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.jsonld",
-        //     "jsonSchemaUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.json",
-        //     "typeName": "xampleSchema2",
-        //     "credentialSubject": {
-        //         "data": {
-        //             "firstname": cert.givenName,
-        //             "lastname": cert.familyName,
-        //             "course": cert.course,
-        //         },
-        //         "holderDid": "did:elem:EiBMNJLGF6ZtwfoMJbS9bclIKB-sgNP1JyiRl9UOLV_OHQ"
-        //     }
-        // }
-        // // axios header configs
-        // const config = {
-        //     headers: {
-        //         'Api-Key': process.env.REACT_APP_API_KEY_HASH,
-        //         'Content-Type': 'application/json',
-        //     }
-        // }
-        // // post it to axios
-        // // @ts-ignore
-        // const {data} = await axios.post(issuerBaseUrl, body, config);
-        // console.log(data);
+        const body = {
+            "jsonLdContextUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.jsonld",
+            "jsonSchemaUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.json",
+            "typeName": "xampleSchema2",
+            "credentialSubject": {
+                "data": {
+                    "firstname": cert.givenName,
+                    "lastname": cert.familyName,
+                    "course": cert.course,
+                },
+                "holderDid": process.env.REACT_APP_DID // the did value is from postman collection in  env variables
+            }
+        }
+        // axios header configs
+        const config = {
+            headers: {
+                'Api-Key': process.env.REACT_APP_API_KEY_HASH,
+                'Content-Type': 'application/json',
+            }
+        }
+        // @ts-ignore
+        const {data} = await axios.post(issuerBaseUrl, body, config);
+        console.log(data);
 
         cert.isApproved = true;
         localStorage.setItem("cert", JSON.stringify(cert));
+
+        //TODO: after i call the build unsign Affinidi API
+        // I nmeed to call the Issuer API(Sign VC)
+
     }
 
     return (
