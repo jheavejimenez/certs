3a8d3f28acd48723a312d6aa6cc66919d3e2b9e0
jheave <jimenezjheave123@gmail.com>
2022-08-03 17:55:43 +0800

    add json server

--- src/Pages/Certificate.tsx
+++ src/Pages/Certificate.tsx
@@ -9,33 +9,14 @@ export const Certificate:React.FC<{}> = () => {
     const data = [JSON.parse(localStorage.getItem("cert") || "{}")];
     const filteredData = data.filter(item => item.isApproved)
 
-    const dummyData = [
-        {
-            "givenName":"jheave",
-            "familyName":"Jimenenez",
-            "course":"CEIT",
-            "isApproved": false
-        },
-        {
-            "givenName":"James",
-            "familyName":"doe",
-            "course":"CEIT",
-            "isApproved": false
-        },
-        {
-            "givenName":"Jhon",
-            "familyName":"doe",
-            "course":"CEIT",
-            "isApproved": false
-        }
-    ]
+
 
     return (
         <Container maxW={'1280px'} marginTop={'10vh'}>
             { !isApproved ?
                 <Card /> :
                 <SimpleGrid columns={3} spacing={5}>
-                    {dummyData.map((cert: any, index: number) => (
+                    {filteredData.map((cert: any, index: number) => (
                         <CertificateCardHolder
                             key={index}
                             firstName={cert.givenName}
