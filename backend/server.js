27a46f4f8bb35935fa0438029131add35f117ff5
jheave <jimenezjheave123@gmail.com>
2022-08-10 15:23:42 +0800

    add mongoose db

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-}).listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+const port = 5000;
 
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
