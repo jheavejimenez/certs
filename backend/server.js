0ad19f473daa9b2094bc4f75dadb2f247471c06e
jheave <jimenezjheave123@gmail.com>
2022-08-10 16:05:01 +0800

    added express js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 require('dotenv').config();
 
 const app = express()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
-// Middleware
-app().use(cors());
-app().use(express.json()); // for parsing application/json
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB connected');
+});
+
+
+// commented this out because it was causing an error
+
+// app().use(cors()); // Middleware
+// app().use(express.json()); // for parsing application/json
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
