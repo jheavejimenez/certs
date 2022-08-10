cefe124217a0758ecac4094bfdb1d569583cc70a
jheave <jimenezjheave123@gmail.com>
2022-08-10 19:06:42 +0800

    refractor

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv')
 dotenv.config({path:__dirname+'/.env'});
 
-
 const app = express()
 const port = process.env.PORT || 4000;
 
+// commented this out because it was causing an error
 app().use(express.json()); // for parsing application/json
 
 const uri = process.env.ATLAS_URI;
@@ -20,10 +20,6 @@ const connection = mongoose.connection;
 connection.once('open', () => {
     console.log('MongoDB connected');
 });
-
-
-// commented this out because it was causing an error
-
 
 const certificateRouter = require('./routes/certificate');
 app.use('/api/certificates', certificateRouter);
