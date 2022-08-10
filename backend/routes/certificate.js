adb2c63ac23184dad09250266a87ac744a0d45a7
jheave <jimenezjheave123@gmail.com>
2022-08-10 17:56:41 +0800

    refractor

--- backend/routes/certificate.js
+++ backend/routes/certificate.js
@@ -2,3 +2,14 @@ const router = require('express').Router();
 
 let Certificate = require('../models/certificate.model');
 
+router.route('/').get(async (req, res) => {
+
+}).post((req, res) => {
+    const certificate = req.body;
+
+    const newCertificate = new Certificate({certificate});
+
+    newCertificate.save()
+        .then(() => res.json(newCertificate))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
