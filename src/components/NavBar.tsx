4576f2d58982e68548ecd60e560c4c30e226b4a7
jheave <jimenezjheave123@gmail.com>
2022-08-22 18:38:13 +0800

    refractor

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,35 +1,70 @@
-import {Avatar, Box, Button, Flex, HStack, Menu, MenuButton, useColorModeValue,} from '@chakra-ui/react';
+import {ReactNode} from 'react';
+import {
+    Avatar,
+    Box,
+    Button,
+    Flex,
+    HStack,
+    Link,
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
+    useColorModeValue,
+} from '@chakra-ui/react';
+
+const Links = ['Home', 'Certificates' ];
+
+const NavLink = ({children}: { children: ReactNode }) => (
+    <Link
+        px={2}
+        py={1}
+        rounded={'md'}
+        _hover={{
+            textDecoration: 'none',
+            bg: useColorModeValue('gray.200', 'gray.700'),
+        }}
+        href={'#'}>
+        {children}
+    </Link>
+);
 
 export default function NavigationBar() {
     return (
-        <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
-            <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                <HStack spacing={8} alignItems={'center'}>
-                    <Box><strong>XPERTO</strong></Box>
-                    <HStack
-                        as={'nav'}
-                        spacing={4}
-                        display={{base: 'none', md: 'flex'}}>
+        <>
+            <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+                    <HStack spacing={8} alignItems={'center'}>
+                        <Box><strong>XPERTO</strong></Box>
+                        <HStack
+                            as={'nav'}
+                            spacing={4}
+                            display={{base: 'none', md: 'flex'}}>
+                            {Links.map((link) => (
+                                <NavLink key={link}>{link}</NavLink>
+                            ))}
+                        </HStack>
                     </HStack>
-                </HStack>
-                <Flex alignItems={'center'}>
-                    <Menu>
-                        <MenuButton
-                            as={Button}
-                            rounded={'full'}
-                            variant={'link'}
-                            cursor={'pointer'}
-                            minW={0}>
-                            <Avatar
-                                size={'sm'}
-                                src={
-                                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                }
-                            />
-                        </MenuButton>
-                    </Menu>
+                    <Flex alignItems={'center'}>
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                minW={0}>
+                                <Avatar
+                                    size={'sm'}
+                                    src={
+                                        'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                    }
+                                />
+                            </MenuButton>
+                        </Menu>
+                    </Flex>
                 </Flex>
-            </Flex>
-        </Box>
+            </Box>
+        </>
     );
 }
