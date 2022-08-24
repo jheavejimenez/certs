4c83c22b77dd8196f00abe9202adc8b50f6cae6b
jheave <jimenezjheave123@gmail.com>
2022-08-24 22:47:54 +0800

    refractor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Certificate from "./pages/Certificate";
-import Approver from "./pages/Approver";
-import SignInForm from "./pages/SignInForm";
 import {UserContext} from "./context/UserContext";
 
 export default function App() {
