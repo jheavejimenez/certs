c984bb52466d0903ab6756ee8dbd8d73990c0415
jheave <jimenezjheave123@gmail.com>
2022-08-24 18:23:35 +0800

    refractor: model schema

--- backend/models/certificate.model.js
+++ backend/models/certificate.model.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const certificateSchema = new Schema({
-    firstName: {type: String, required: true},
-    lastName: {type: String, required: true},
-    course: {type: String, required: true},
-    isApprove: {type: Boolean},
-    claimId: {type: String}
+    userName: {type: String, required: true},
+    userEmail: {type: String, required: true},
+    certificates:[{
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true},
+        course: {type: String, required: true},
+        isApprove: {type: Boolean},
+        claimId: {type: String}
+    }]
 }, {
     timestamps: true,
 })
--- src/utils/schemaVC.ts
+++ src/utils/schemaVC.ts
@@ -1,6 +1,4 @@
-export const schoolSchema = (
-    {firstName, lastName, course}: { firstName: string; lastName: string; course: string }
-) => {
+export const schoolSchema = (firstName: string, lastName: string, course: string) => {
     return {
         "jsonLdContextUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.jsonld",
         "jsonSchemaUrl": "https://schema.affinidi.com/@did:elem:EiC4iIPkKE9Emed3MbAqZ8z8QixcKFPtSoUUSpSP1XA4aA/xampleSchema2V1-0.json",
