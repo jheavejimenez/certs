754acbec3f085d20622a1d4a067e6fe31c2471d5
jheave <jimenezjheave123@gmail.com>
2022-08-24 18:33:14 +0800

    refractor: model schema

--- backend/models/certificate.model.js
+++ backend/models/certificate.model.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const certificateSchema = new Schema({
-    userName: {type: String, required: true},
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3,
+    },
     userEmail: {type: String, required: true},
     certificates:[{
         firstName: {type: String, required: true},
