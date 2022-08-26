763de69e561a598226fede3b602c70e26a923e2e
jheave <jimenezjheave123@gmail.com>
2022-08-26 23:07:39 +0800

    refactor

--- backend/routes/certificate.js
+++ backend/routes/certificate.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 let Certificate = require('../models/certificate.model');
-const axios = require("axios");
-
 
 router.get('/', async (req, res) => {
     const certificates = await Certificate.find({isApprove: true})
@@ -18,42 +16,5 @@ router.route('/:id').get(async (req, res) => {
         .then(() => res.json(newCertificate))
         .catch(err => res.status(500).json(`error ${err}`));
 });
-
-const login = async () => {
-    const login = await axios.post(`${server.affinidi}/users/login`, {
-        username: process.env.USERNAME,
-        password: process.env.PASSWORD
-
-    }, {
-        headers: {"Content-Type": "application/json", "Api-Key": process.env.REACT_APP_API_KEY_HASH}
-    })
-    return login.data.accessToken
-}
-
-const signVc = async (accessToken, data) => {
-    const sign = await axios.post(`${server.affinidi}/wallet/sign-credential`,{"unsignedCredential": data}, {
-        headers: {
-            "Content-Type": "application/json",
-            "Api-Key": process.env.REACT_APP_API_KEY_HASH,
-            "Authorization": `Bearer ${accessToken}`
-        }
-    })
-    return sign.data.signedCredential.id
-}
-
-router.route('/:id').put(async (req, res) => {
-    try {
-        const {firstName, lastName, email, course, isApprove, unsignedCredentials} = req.body;
-        const accessToken = await login()
-        const claimId = await signVc(accessToken, unsignedCredentials)
-
-        const update = {firstName, lastName, email, course, isApprove, claimId};
-        const updatedCertificate = await Certificate.findByIdAndUpdate(req.params.id, update, {new: true});
-        res.json(updatedCertificate)
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(`error ${err}`)
-    }
-})
 
 module.exports = router;
