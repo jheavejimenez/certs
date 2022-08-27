f1f5858d03e32e767282c1c9e702a994b5d96a8c
jheave <jimenezjheave123@gmail.com>
2022-08-27 14:19:33 +0800

    add condition to sign in

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,7 @@
 import {Avatar, Box, Button, Flex, HStack, Menu, MenuButton, useColorModeValue,} from '@chakra-ui/react';
 import {Link} from "react-router-dom";
+import React from "react";
+import {UserContext} from "../context/UserContext";
 
 const Links = {
     Dashboard: {
@@ -11,6 +13,7 @@ const Links = {
 }
 
 export default function NavigationBar() {
+    const {user} = React.useContext(UserContext);
     return (
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
@@ -30,21 +33,31 @@ export default function NavigationBar() {
                         </HStack>
                     </HStack>
                     <Flex alignItems={'center'}>
-                        <Menu>
-                            <MenuButton
-                                as={Button}
-                                rounded={'full'}
-                                variant={'link'}
-                                cursor={'pointer'}
-                                minW={0}>
-                                <Avatar
-                                    size={'sm'}
-                                    src={
-                                        'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                                    }
-                                />
-                            </MenuButton>
-                        </Menu>
+                        {user.id ? (
+                            <Menu>
+                                <MenuButton
+                                    as={Button}
+                                    rounded={'full'}
+                                    variant={'link'}
+                                    cursor={'pointer'}
+                                    minW={0}>
+                                    <Avatar
+                                        size={'sm'}
+                                        src={
+                                            'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                        }
+                                    />
+                                </MenuButton>
+                            </Menu>
+                        ) : (
+                            <Button
+                                as={Link}
+                                to="/sign-in"
+                            >
+                                Sign In
+                            </Button>
+                        )
+                        }
                     </Flex>
                 </Flex>
             </Box>
